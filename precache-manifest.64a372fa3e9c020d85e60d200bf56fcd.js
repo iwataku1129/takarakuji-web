929b9ecf57c5d635d90eaadf0eec42f62edcb0e5
Takuma Iwai <t.iwai@shachi-bus.co.jp>
2022-01-11 11:41:39 +0900

    Updates

---
+++ precache-manifest.64a372fa3e9c020d85e60d200bf56fcd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96439e9cfe0b9dcca89a0d0f971efe77",
+    "url": "/takarakuji-web/index.html"
+  },
+  {
+    "revision": "19e784d26d22af83be48",
+    "url": "/takarakuji-web/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "71c5f52abbad3fc2f811",
+    "url": "/takarakuji-web/static/css/main.1267a2e1.chunk.css"
+  },
+  {
+    "revision": "19e784d26d22af83be48",
+    "url": "/takarakuji-web/static/js/2.504e9a6c.chunk.js"
+  },
+  {
+    "revision": "f88a53c7557600b94f2a",
+    "url": "/takarakuji-web/static/js/3.3333c741.chunk.js"
+  },
+  {
+    "revision": "71c5f52abbad3fc2f811",
+    "url": "/takarakuji-web/static/js/main.c23987fa.chunk.js"
+  },
+  {
+    "revision": "2ce2069237ee70afe840",
+    "url": "/takarakuji-web/static/js/runtime~main.95dedb7b.js"
+  },
+  {
+    "revision": "6acae9c3873b515ca4f433fa112c479d",
+    "url": "/takarakuji-web/static/media/animation.6acae9c3.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.40abb9a70216bae21524b2881e5d83ce.js"
+  "/takarakuji-web/precache-manifest.64a372fa3e9c020d85e60d200bf56fcd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/takarakuji-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
